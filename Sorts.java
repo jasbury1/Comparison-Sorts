345a0cde1e5a9db2367bdbb861a74eebe833d8be
James Asbury <jamesasbury03@gmail.com>
2019-01-11 13:54:15 -0800

    added merge helper for merge function

--- Sorts.java
+++ Sorts.java
@@ -11,33 +11,90 @@ class Sorts{
 
    //Returns number of comparisons
    public static long insertSort(int[] data){
-      return 4;
+      long comparisons = 0;
+
+      for(int i = 1; i < data.length; i++){
+         //j represents an index to check to find insertion location
+         int j = i;
+         while(j > 0 && data[j - 1] > data[j]){
+            ++comparisons;
+            int temp = data[j];
+            data[j] = data[j - 1];
+            data[j - 1] = temp;
+            --j;
+         }
+      }
+      return comparisons;
    }
 
    //Returns number of comparisons
    public static long selectSort(int[] data){
       //min refers to an index of the smalles data member found
       int min;
+      long comparisons = 0;
+
       for(int i = 0; i < data.length - 1; i++){
          min = i;
+
          for(int j = i + 1; j < data.length; j++){
             //index through the remainder of the array in search of new smaller min value
+            ++comparisons;
             if(data[j] < data[min]){
                //update min with new smallest value
                min = j;
             }
          }
+
          int temp = data[i];
          data[i] = data[min];
          data[min] = temp;
       }
-      System.out.println("RESULT: " + Arrays.toString(data));
-      return 4;
+      return comparisons;
    }
 
    //Returns number of comparisons
    public static long mergeSort(int[] data){
       return 4;
+   }
+
+   //Merges two sorted arrays into one sorted array
+   //return the merged array
+   public static int[] merge(int[] arrA, int[] arrB){
+      int[] result = new int[arrA.length + arrB.length];
+      
+      int i = 0;
+      int j = 0;
+      int k = 0;
+
+      while(i < arrA.length && j < arrB.length){
+         //test whether we add an element from arrayA or arrayB to the result
+         if(arrA[i] <= arrB[j]){
+            result[k] = arrA[i];
+            ++i;
+         }
+         else{
+            result[k] = arrB[j];
+            ++j;
+         }
+         //move onto next index of the result
+         ++k;
+      }
+      
+      //if we are at the end of A, then copy the rest of B to result
+      if(i == arrA.length){
+         for(; j < arrB.length; j++){
+            result[k] = arrB[j];
+            ++k;
+         }
+      }
+      //if we are at the end of B, then copy the rest of A to result
+      else{
+         for(; i < arrA.length; i++){
+            result[k] = arrA[i];
+            ++k;
+         }
+      }
+      return result;
    }
 
 
--- SortsTestTemplate.java
+++ SortsTestTemplate.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 public class SortsTestTemplate	{		
 	
 	public static void main(String[]	args)	 {	
-      int[] a= {10,1,2,3,4,5,6,7,8,9}; 
+      int[] a= {10,1,2,3,4,5,6,7,8,9, 5, 78, 3, 2, 5, 7, 2, 546, 6, 23, -5, -6, 23, 324, 7, 34, 2,65, 3, 4, 14, 22, -3}; 
   		int[] temp	= a.clone();
 		System.out.println("test array to sort:  " + Arrays.toString(a));
 	   long numComp = Sorts.insertSort (temp);
@@ -32,6 +32,11 @@ public class SortsTestTemplate	{
 		System.out.println("Sorted by merge sort: ");
       System.out.println(Arrays.toString(temp));
 		System.out.println("# of comparisons by mergesort is = " +	numComp);
-		System.out.println();	 
+		System.out.println();
+
+                System.out.println("TESTING MERGE FUNCTION: ");
+                int[] arr1 = {1, 3, 5, 7, 9, 11, 12, 15};
+                int[] arr2 = { 2, 4, 6, 8, 10};
+                System.out.println(Arrays.toString(Sorts.merge(arr1, arr2)));
 	}
 }
